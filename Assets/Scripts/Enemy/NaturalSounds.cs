07c172f04dc87ee848641ab41ff0cc8410d2220b
Yaroslav Rezchikov <rezchikov.md@gmail.com>
2022-06-24 15:55:04 +0300

    feat: post processing, effects

---
+++ Assets/Scripts/Enemy/NaturalSounds.cs
@@ -0,0 +1,33 @@
+using System;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+public class NaturalSounds : MonoBehaviour
+{
+    [SerializeField] private AudioClip[] ghoulNaturalSounds;
+    [SerializeField] private float naturalSoundsOffset = 0.5f;
+
+    private AudioSource _ghoulNaturalSounds;
+    private float _naturalSoundsTimer = 0;
+    
+
+    private void Awake()
+    {
+        _ghoulNaturalSounds = GetComponent<AudioSource>();
+    }
+
+    private void Update()
+    {
+        PlayNaturalSounds();
+    }
+
+    private void PlayNaturalSounds()
+    {
+        _naturalSoundsTimer -= Time.deltaTime;
+        if (_naturalSoundsTimer <= 0)
+        {
+            _ghoulNaturalSounds.PlayOneShot(ghoulNaturalSounds[Random.Range(0, ghoulNaturalSounds.Length - 1)]);
+            _naturalSoundsTimer = naturalSoundsOffset;
+        }
+    }
+}
