80d2d142d0e9f6c80155fdf0590e7e264ed7c043
Yaroslav Rezchikov <rezchikov.md@gmail.com>
2022-06-29 13:54:48 +0300

    shooting dynamic

--- Assets/Scripts/PlayerMovement/Weapon.cs
+++ Assets/Scripts/PlayerMovement/Weapon.cs
@@ -12,11 +12,18 @@ public class Weapon : MonoBehaviour
     [SerializeField] private float damage = 10;
     [SerializeField] private float fireRate = 1;
     [SerializeField] private float range = 30;
+    [SerializeField] private int maxAmmo = 30;
+    public int ammoLoaded = 30;
+    private bool _ammo;
+    public bool canShoot;
+    public static Action <int,int> AmmoChanged;
+    public static Action IsReloading;
     [SerializeField] private ParticleSystem muzzleFlash;
     [SerializeField] private Transform bulletSpawner;
     [SerializeField] private AudioClip fire;
     [SerializeField] private AudioClip reload;
     [SerializeField] private AudioClip noAmmo;
+    [SerializeField] private Light flashLight;
     private AudioSource _weaponAudioSource;
     private Camera _camera;
     public bool CanShoot { get; set;}
@@ -30,23 +37,25 @@ public class Weapon : MonoBehaviour
         flash = GetComponentInChildren<Light>();
         _weaponAudioSource = GetComponent<AudioSource>();
     }
-
-    void Start()
-    {
-        
-    }
-
+    
     void Update()
     {
-        if (Input.GetKey(KeyCode.Mouse0) && !_shootCooling)
+        if (canShoot)
         {
-            Shoot();
+            if (ammoLoaded !=maxAmmo && Input.GetKeyDown(KeyCode.R))
+                Reload();
+            else if(ammoLoaded == 0 && !_shootCooling && Input.GetKey(KeyCode.Mouse0))
+                NoAmmo();
+            else if(Input.GetKey(KeyCode.Mouse0) && !_shootCooling)
+                Shoot();
         }
     }
 
     // ReSharper disable Unity.PerformanceAnalysis
     private void Shoot()
     {
+        ammoLoaded -= 1;
+        AmmoChanged?.Invoke(ammoLoaded,maxAmmo);
         _shootCooling = true;
         _weaponAudioSource.PlayOneShot(fire,3);
         muzzleFlash.Play();
@@ -64,10 +73,38 @@ public class Weapon : MonoBehaviour
 
     private IEnumerator GunShotFlash()
     {
+        _camera.transform.Rotate(_camera.transform.right,2);
         flash.enabled = true;
         yield return new WaitForSeconds(fireRate);
         flash.enabled = false;
-        //yield return new WaitForSeconds(fireRate);
         _shootCooling = false;
+    }
+
+    private void NoAmmo()
+    {
+        StartCoroutine(NoAmmoCoroutine());
+    }
+
+    IEnumerator NoAmmoCoroutine()
+    {
+        _shootCooling = true;
+        _weaponAudioSource.PlayOneShot(noAmmo);
+        yield return new WaitForSeconds(fireRate * 4);
+        _shootCooling = false;
+    }
+
+    private void Reload()
+    {
+        IsReloading?.Invoke();
+        StartCoroutine(ReloadGun());
+    }
+
+    private IEnumerator ReloadGun()
+    {
+        _weaponAudioSource.PlayOneShot(reload);
+        yield return new WaitForSeconds(reload.length);
+        ammoLoaded = maxAmmo;
+        flashLight.enabled = true;
+        AmmoChanged?.Invoke(ammoLoaded,maxAmmo);
     }
 }
