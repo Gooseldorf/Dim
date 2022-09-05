1a7213a5e23ae0071d7ae9915194fcab4a549a58
MuninYar <rezchikov.md@gmail.com>
2022-09-05 15:53:57 +0300

    Bug fixes

--- Assets/Scripts/Level2/Level2Handler.cs
+++ Assets/Scripts/Level2/Level2Handler.cs
@@ -7,7 +7,7 @@ public class Level2Handler : MonoBehaviour
     public static Action Level2started;
     [SerializeField] private AudioSource ambientAudioSource;
 
-    private void Awake()
+    private void Start()
     {
         Level2started?.Invoke();
         ambientAudioSource.volume = 0.3f;
